acdc1c694ac196101f74cd335253b74140de0f53
nrin31266 <162881731+nrin31266@users.noreply.github.com>
2024-10-26 18:53:14 +0700

    sửa config

--- src/configurations/configurations.ts
+++ src/configurations/configurations.ts
@@ -1,5 +1,5 @@
 export const API = {
-  USER: "/identity/users",
+  USERS: "/identity/users",
   USER_INFO: "/identity/users/info",
   LOGIN: "/identity/auth/login",
   LOGOUT: "/identity/auth/logout",
@@ -8,7 +8,7 @@ export const API = {
   REFRESH_TOKEN: "/identity/auth/refresh",
   VERIFY_TOKEN:'identity/auth/introspect',
   CREATE_SUPPLIER: "/kanban/suppliers/create",
-  SUPPLIER:'/kanban/suppliers',
+  SUPPLIERS:'/kanban/suppliers',
   GET_SUPPLIERS: (page:number|string,size: number|string) => `/kanban/suppliers?page=${page}&size=${size}`,
   UPDATE_SUPPLIER: (suppliersId:string) => `/kanban/suppliers/${suppliersId}`,
   DELETE_SUPPLIER: (suppliersId:string) => `/kanban/suppliers?suppliersId=${suppliersId}`,
@@ -26,5 +26,6 @@ export const appInfos = {
   logo: "https://firebasestorage.googleapis.com/v0/b/kanban-ac9c5.appspot.com/o/kanban-logo.png?alt=media&token=b72b8db5-b31d-4ae9-aab8-8bd7e10e6d8e",
   title: "KANBAN",
   description: "",
+
 };
 
