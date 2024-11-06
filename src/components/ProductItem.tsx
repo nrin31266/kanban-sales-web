f4663bdad36b29f37a8bedc2976b005ce90b9266
nrin31266 <162881731+nrin31266@users.noreply.github.com>
2024-11-06 23:16:02 +0700

    UI

--- src/components/ProductItem.tsx
+++ src/components/ProductItem.tsx
@@ -30,69 +30,33 @@ const ProductItem = (props: Props) => {
       className="col-6 col-md-4 col-lg-3 product-item p-2"
     >
       {product.images && product.images.length > 0 ? (
-        <div className="p-0" style={{ backgroundColor: "#e0e0e0" }}>
-          <div
-            className="image mt-1"
-            style={{
-              height: elementWidth ? elementWidth * 1 : 250,
-              width: "100%",
-              background: `url('${product.images[0]}')`,
-              backgroundRepeat: "no-repeat",
-              backgroundSize: "cover",
-              border: "4px",
-            }}
-            // src={'https://th.bing.com/th/id/OIP.qeI5ebWY0xxZdCGUMxrzWAHaNK?rs=1&pid=ImgDetMain'}
-          >
-            <div
-              className="image-content"
-              style={{
-                display: "flex",
-                flexDirection: "column",
-                height: "100%",
-              }}
-            >
-              <div style={{ flex: 1 }} className="text-right mr-1">
-                <div>
-                  <Button
-                   onClick={(e) => e.stopPropagation()}
-                    style={{ borderRadius: "50%" }}
-                    className="mb-2 btn-icon mt-2"
-                    icon={<FcLike size={20} />}
-                  ></Button>
-                </div>
-                <div>
-                  <Button
-                   onClick={(e) => e.stopPropagation()}
-                    style={{ borderRadius: "50%" }}
-                    className="mb-2 btn-icon"
-                    icon={<BiTransfer size={20} />}
-                  ></Button>
-                </div>
-                <div>
-                  <Button
-                   onClick={(e) => e.stopPropagation()}
-                    style={{ borderRadius: "50%" }}
-                    className="mb-2 btn-icon"
-                    icon={<IoMdAdd size={20} />}
-                  ></Button>
-                </div>
-              </div>
-              <div className="text-center mb-2">
-                <Button  onClick={(e) => {
-                    e.stopPropagation();
-                    console.log('view');
-                }} style={{ width: "50%" }}>
-                  <FaEye /> View
-                </Button>
-              </div>
+        <>
+          <div className="image" style={{ backgroundColor: "#e0e0e0" }}>
+            <img
+              width={"100%"}
+              height={elementWidth ? elementWidth * 1.1 : 250}
+              src="https://i.pinimg.com/736x/47/50/22/47502277fa068232f5a3556f18c362a2.jpg"
+              alt={product.slug}
+            />
+          </div>
+          <div className="hover-content" style={{
+            
+          }}> 
+            <div className="top-right">
+              <Button>J</Button> <br />
+              <Button>J</Button> <br />
+              <Button>J</Button>
+            </div>
+            <div className="bottom">
+              <Button style={{width: '50%'}}>J đó</Button>
             </div>
           </div>
-        </div>
+        </>
       ) : (
         <div
           style={{
             width: "100%",
-            height: elementWidth ? elementWidth * 1 : 250,
+            height: elementWidth ? elementWidth * 1.1 : 250,
             backgroundColor: `#e0e0e0`,
             display: "flex",
             justifyContent: "center",
