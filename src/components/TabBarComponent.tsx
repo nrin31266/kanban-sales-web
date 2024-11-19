c5ac87fb12bcda3692fe45c78740dc4ffe49a496
nrin31266 <162881731+nrin31266@users.noreply.github.com>
2024-11-20 01:57:23 +0700

    UI

--- src/components/TabBarComponent.tsx
+++ src/components/TabBarComponent.tsx
@@ -1,29 +1,27 @@
-import { Typography } from 'antd';
-import React, { ReactNode } from 'react'
+import { Typography } from "antd";
+import React, { ReactNode } from "react";
 
-interface Props{
-    titleAlign?: 'text-center' | 'text-right' | 'text-left';
-    title: string;
-    titleLevel: 1 | 2 | 3 | 4 | 5 | undefined;
-    titleRight?: ReactNode
-    children: ReactNode;
+interface Props {
+  titleAlign?: "text-center" | "text-right" | "text-left";
+  title: string;
+  titleLevel: 1 | 2 | 3 | 4 | 5 | undefined;
+  titleRight?: ReactNode;
+  children: ReactNode;
 }
 
-const TabBarComponent = (props : Props) => {
-    const {title, titleAlign, titleLevel, titleRight, children} = props;
+const TabBarComponent = (props: Props) => {
+  const { title, titleAlign, titleLevel, titleRight, children } = props;
   return (
-    <div style={{backgroundColor: 'white'}}>
-        <div className="row" style={{marginLeft: '0px !important', marginRight: '0px !important'}} >
-            <div className={`${titleRight? 'col-9' : 'col'} ${titleAlign}`}>
-                <Typography.Title level={titleLevel}>{title} </Typography.Title>
-            </div>
-            {titleRight && <div className='col-3 text-right'>{titleRight}</div>}
+    <div className="" style={{ backgroundColor: "white"}}>
+      <div className="row">
+        <div className={`${titleRight ? "col-9" : "col"} ${titleAlign}`}>
+          <Typography.Title level={titleLevel}>{title} </Typography.Title>
         </div>
-        <div>
-            {children}
-        </div>
+        {titleRight && <div className="col-3 text-right">{titleRight}</div>}
+      </div>
+      <div>{children}</div>
     </div>
-  )
-}
+  );
+};
 
-export default TabBarComponent
+export default TabBarComponent;
--- src/pages/products/[productId]/[slug]/index.tsx
+++ src/pages/products/[productId]/[slug]/index.tsx
@@ -5,7 +5,7 @@ import { API, APP, PAGE } from "@/configurations/configurations";
 import { CustomAxiosResponse } from "@/model/AxiosModel";
 import { ProductResponse } from "@/model/ProductModel";
 import { FormatCurrency } from "@/utils/formatNumber";
-import { Breadcrumb, Empty, Rate, Space, Tag, Typography } from "antd";
+import { Breadcrumb, Empty, Image, Rate, Space, Tabs, Tag, Typography } from "antd";
 import axios from "axios";
 import Link from "next/link";
 import { useState, useMemo, useCallback } from "react";
@@ -55,6 +55,7 @@ const ProductDetail = ({
             url={`${APP.baseURL}/products/${product.id}/${product.slug}`}
           />
           <div className="container mt-3 product-detail">
+            <div className="bg-white">
             <Breadcrumb
               items={[
                 { key: "home", title: <Link href={PAGE.HOME}>Home</Link> },
@@ -62,13 +63,14 @@ const ProductDetail = ({
                 { key: "product", title: product.title },
               ]}
             />
+            </div>
             <div className="row mt-3" style={{ backgroundColor: "white" }}>
               <div className="col-sm-12 col-md-4">
                 <div
                   className="text-center p-4"
                   style={{ backgroundColor: "#e0e0e0" }}
                 >
-                  <img src={photoSelected} width="100%" alt="Product image" />
+                  <Image src={photoSelected} width="100%" alt="Product image" />
                 </div>
                 <ScrollItems
                   onClick={() => {}}
@@ -146,7 +148,58 @@ const ProductDetail = ({
                 />
               </div>
             </div>
-            <Section children={<>Longer content to fill space</>} />
+            <Section
+              children={
+                <div className="bg-white">
+                  <Tabs
+                    items={[
+                      {
+                        key: "tab-1",
+                        label: "Description",
+                        children: (
+                          <>
+                            Lorem ipsum dolor sit amet consectetur adipisicing
+                            elit. Possimus ducimus dolore nesciunt cupiditate
+                            error adipisci minus, culpa excepturi! Quas
+                            necessitatibus numquam inventore a sapiente saepe
+                            dolores officiis error est reprehenderit!
+                          </>
+                        ),
+                        
+                      },
+                      {
+                        key: "tab-2",
+                        label: "Additional Ifnomations",
+                        children: (
+                          <>
+                            Lorem ipsum dolor sit amet consectetur adipisicing
+                            elit. Possimus ducimus dolore nesciunt cupiditate
+                            error adipisci minus, culpa excepturi! Quas
+                            necessitatibus numquam inventore a sapiente saepe
+                            dolores officiis error est reprehenderit!
+                          </>
+                        ),
+                        
+                      },
+                      {
+                        key: "tab-3",
+                        label: "Reviews",
+                        children: (
+                          <>
+                            Lorem ipsum dolor sit amet consectetur adipisicing
+                            elit. Possimus ducimus dolore nesciunt cupiditate
+                            error adipisci minus, culpa excepturi! Quas
+                            necessitatibus numquam inventore a sapiente saepe
+                            dolores officiis error est reprehenderit!
+                          </>
+                        ),
+                        
+                      },
+                    ]}
+                  />
+                </div>
+              }
+            />
             <Section
               children={
                 <TabBarComponent
